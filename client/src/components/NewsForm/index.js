482c7d3195979ae19939c57e71e69ae6b0448436
ReTims <renatatims@gmail.com>
2023-02-23 14:33:38 -0600

    Included upload + photo icon - so user can upload images to the message

--- client/src/components/NewsForm/index.js
+++ client/src/components/NewsForm/index.js
@@ -2,6 +2,12 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
+//Import buttons
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Stack from "@mui/material/Stack";
+
 export default function FullWidthTextField() {
   return (
     <Box
@@ -29,6 +35,27 @@ export default function FullWidthTextField() {
         multiline
         rows={8}
       />
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={2}
+        sx={{
+          p: 1,
+        }}
+      >
+        <Button variant="contained" component="label">
+          Upload
+        </Button>
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="label"
+        >
+          <input hidden accept="image/*" type="file" />
+          <PhotoCamera />
+        </IconButton>
+        
+      </Stack>
     </Box>
   );
 }
