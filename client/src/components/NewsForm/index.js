ba33b686786d36f0d834a13f71d9c1ae83108b1e
ReTims <renatatims@gmail.com>
2023-02-27 21:57:15 -0600

    Included handleChange and handleFormSubmit to the form and inputs

--- client/src/components/NewsForm/index.js
+++ client/src/components/NewsForm/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
@@ -7,55 +7,82 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
 
-export default function FullWidthTextField() {
+const NewsForm = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    message: "",
+  });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    if (event.target.value.length) {
+      setFormState({ ...formState, [event.target.name]: event.target.value });
+    }
+  };
+
+  // submit form
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <Box
-      sx={{
-        width: 500,
-        maxWidth: "100%",
-        p: 4,
-      }}
-    >
-      <TextField
-        fullWidth
-        label="Name"
-        id="fullWidth"
+    <FormControl onSubmit={handleFormSubmit}>
+      <Box
         sx={{
-          p: 1,
-        }}
-      />
-      <TextField
-        fullWidth
-        label="Message"
-        id="fullWidth"
-        sx={{
-          p: 1,
-        }}
-        multiline
-        rows={8}
-      />
-      <Stack
-        direction="row"
-        alignItems="center"
-        spacing={2}
-        sx={{
-          p: 1,
+          width: 500,
+          maxWidth: "100%",
+          p: 4,
         }}
       >
-        <Button variant="contained" component="label">
-          Upload
-        </Button>
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="label"
+        <TextField
+          fullWidth
+          label="Name"
+          name="username"
+          value={formState.username}
+          id="fullWidth"
+          sx={{
+            p: 1,
+          }}
+          onChange={handleChange}
+        />
+        <TextField
+          fullWidth
+          label="Message"
+          name="message"
+          value={formState.message}
+          id="fullWidth"
+          sx={{
+            p: 1,
+          }}
+          multiline
+          rows={8}
+          onChange={handleChange}
+        />
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+          sx={{
+            p: 1,
+          }}
         >
-          <input hidden accept="image/*" type="file" />
-          <PhotoCamera />
-        </IconButton>
-        
-      </Stack>
-    </Box>
+          <Button variant="contained" component="label" type="submit">
+            Submit
+          </Button>
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="label"
+          >
+            <input hidden accept="image/*" type="file" />
+            <PhotoCamera />
+          </IconButton>
+        </Stack>
+      </Box>
+    </FormControl>
   );
-}
+};
+
+export default NewsForm;
