42594849aa6c96d29cfa4ddec0af1f88bf1b9489
ReTims <renatatims@gmail.com>
2023-02-27 22:35:59 -0600

    Added character count to message and name - handleChange

--- client/src/components/NewsForm/index.js
+++ client/src/components/NewsForm/index.js
@@ -14,74 +14,84 @@ const NewsForm = () => {
     username: "",
     message: "",
   });
+  //Count characters
+  const [characterCount, setCharacterCount] = useState(0);
 
   // update state based on form input changes
   const handleChange = (event) => {
-    if (event.target.value.length) {
+    if (event.target.value.length <= 250) {
       setFormState({ ...formState, [event.target.name]: event.target.value });
+      setCharacterCount(event.target.value.length);
     }
   };
 
   // submit form
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    setFormState({ username: "", message: "" });
+    setCharacterCount(0);
   };
 
   return (
-    <FormControl onSubmit={handleFormSubmit}>
-      <Box
-        sx={{
-          width: 500,
-          maxWidth: "100%",
-          p: 4,
-        }}
-      >
-        <TextField
-          fullWidth
-          label="Name"
-          name="username"
-          value={formState.username}
-          id="fullWidth"
+    <div>
+      <p >
+        Character Count: {characterCount}/250
+      </p>
+      <FormControl onSubmit={handleFormSubmit}>
+        <Box
           sx={{
-            p: 1,
-          }}
-          onChange={handleChange}
-        />
-        <TextField
-          fullWidth
-          label="Message"
-          name="message"
-          value={formState.message}
-          id="fullWidth"
-          sx={{
-            p: 1,
-          }}
-          multiline
-          rows={8}
-          onChange={handleChange}
-        />
-        <Stack
-          direction="row"
-          alignItems="center"
-          spacing={2}
-          sx={{
-            p: 1,
+            width: 500,
+            maxWidth: "100%",
+            p: 4,
           }}
         >
-          <Button variant="contained" component="label" type="submit">
-            Submit
-          </Button>
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
+          <TextField
+            fullWidth
+            label="Name"
+            name="username"
+            value={formState.username}
+            id="fullWidth"
+            sx={{
+              p: 1,
+            }}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            label="Message"
+            name="message"
+            value={formState.message}
+            id="fullWidth"
+            sx={{
+              p: 1,
+            }}
+            multiline
+            rows={8}
+            onChange={handleChange}
+          />
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={2}
+            sx={{
+              p: 1,
+            }}
           >
-            <input hidden accept="image/*" type="file" />
-            <PhotoCamera />
-          </IconButton>
-        </Stack>
-      </Box>
-    </FormControl>
+            <Button variant="contained" component="label" type="submit">
+              Submit
+            </Button>
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            >
+              <input hidden accept="image/*" type="file" />
+              <PhotoCamera />
+            </IconButton>
+          </Stack>
+        </Box>
+      </FormControl>
+    </div>
   );
 };
 
