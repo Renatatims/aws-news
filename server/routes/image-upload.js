4e401267f90da1a972e59d98aeb7f0ea9651da72
ReTims <renatatims@gmail.com>
2023-02-28 16:46:40 -0600

    Post image route - set up params config + set up S3 service call + dotenv package installed

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const AWS = require('aws-sdk');
+//Import params-config.js file
+const paramsConfig = require('../utils/params-config');
 
 //Instantiate the service object - s3 - to communicate with the S3 web service - so the image can be uploaded to the S3 bucket
 const s3 = new AWS.S3({
@@ -20,5 +22,15 @@ const upload = multer({ storage }).single('image');
 
 router.post('/image-upload', upload, (req, res) => {
     // set up params config
+    const params = paramsConfig(req.file);
     // set up S3 service call
+    s3.upload(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        }
+        res.json(data);
+      });
   });
+
+  module.exports = router;
